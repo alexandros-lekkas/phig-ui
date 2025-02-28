a5f81c6802ac8814c378260330edda4c44dcbbec
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-02-28 17:28:35 -0600

    fix: remove Hello World header from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Button } from "../components/ui/button";
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-4xl font-bold text-center">Hello, World!</h1>
       <Button>See More</Button>
     </div>
   );
