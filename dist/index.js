016b22e7608a499ff6693dfadcab476e5e385eef
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-03-01 14:50:41 -0600

    feat: Implement Button component and update build script to use custom tsconfig

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Button = exports.dog = void 0;
+exports.dog = "dog";
+var button_1 = require("./components/button");
+Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return button_1.Button; } });
